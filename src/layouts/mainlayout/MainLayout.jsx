279e065f3796b28e592e6a5b5054d1aebdb48e4b
javier <javierse9@gmail.com>
2025-05-07 11:54:49 -0300

    register section done

--- src/layouts/mainlayout/MainLayout.jsx
+++ src/layouts/mainlayout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import Nav from "../../components/nav/Nav";
+import Footer from "../../components/footer/Footer";
 export default function MainLayout() {
     return (
         <>
             <Nav/>
             <Outlet/>
+            <Footer/>
         </>
     );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,37 @@
+import './Register.css';
 
 export default function Register() {
     return (
         <>
-            <p>register</p>
+            <main>
+                <section className="register-section">
+                    <div className="main-title">
+                        <h2>Register</h2>
+                        <div>
+                            <h3>¿Do you already have an account?</h3>
+                            <a href="#">Login</a>
+                        </div>
+                    </div>
+                    <div className="register-form">
+                        <form>
+                            <div>
+                                <label htmlFor="email" name="email">Enter your email address</label>
+                                <input id="email" type="email" required />
+
+                                <label htmlFor="username" name="username">Enter your username</label>
+                                <input id="username" type="username" required />
+                                
+                                <label htmlFor="password" name="password">Create a password</label>
+                                <input id="password" type="password" required />
+
+                                <label htmlFor="repeat-password" name="repeat-password">Repeat the password</label>
+                                <input id="repeat-password" type="repeat-password" required />
+                            </div>
+                            <button type="button"><span>Register</span></button>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
